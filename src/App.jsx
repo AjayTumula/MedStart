9a56b071c317d3127717959b63b1ab1184d28077
AjayTumula <ajaytumula72@gmail.com>
2024-05-12 10:39:19 +0100

    Added all the dependencies and fetched all hospital name from the API

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,37 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import medStartLogo from './assets/logomed.png';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <div>
       <AppBar position="static" style={{background: 'white'}}>
-      <Container maxWidth="xl" sx={{ml: 0}}>
-        <Toolbar disableGutters >
-          <img src={medStartLogo} style={{marginRight: 20}} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            // href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'sans-serif',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
-          >
-           MedStart
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        <Container maxWidth="xl" sx={{ml: 0}}>
+          <Toolbar disableGutters >
+            <img src={medStartLogo} style={{marginRight: 20}} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                fontFamily: 'sans-serif',
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'black',
+                textDecoration: 'none',
+              }}
+            >
+            MedStart
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-     <Hospital />
-    </div>
-   
+     <Home />
+    </div>  
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-const Hospital = () => {
+
+
+const Home = () => {
+
+    const [clinics, setClinics] = useState([]);
+
+
+    useEffect(() => {
+        const GEO_API_URL = `https://api.geoapify.com/v2/places?categories=healthcare.hospital&filter=circle:78.4740613,17.360589,5000&bias=proximity:78.4740613,17.360589&limit=20&apiKey=8d3bbb9cb56248728d4751eb24464a1b`;
+        axios.get(GEO_API_URL).then(res => {
+            const featureArr = res.data.features;
+            const name = [];
+            featureArr.map((feature) => name.push(feature.properties.name));
+            setClinics(name);
+            console.log(name);
+        })
+    }, [])
+
+
+
     return(
-        <div></div>
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
+        {
+            clinics.map((clinic, index) => {
+                return (
+                    <div key={index} style={{padding: 20}}>
+                    <Card sx={{ maxWidth: 385, mt: 5, border: '0.2px solid gray' }}>
+                        <CardActionArea>                    
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {clinic}
+                                </Typography>
+                                <hr />
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                    </div>
+                )}
+        )}     
+        </div>
     )           
 }
 
-export default Hospital;
+export default Home;
