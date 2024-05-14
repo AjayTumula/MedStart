733a2215a43959a3df95143aacd04f3661b201bf
AjayTumula <ajaytumula72@gmail.com>
2024-05-14 16:02:30 +0530

    Implemented the route map for user and selected hospital

--- src/components/SelectHospital.jsx
+++ src/components/SelectHospital.jsx
@@ -5,25 +5,28 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 const SelectHospital = ({latLng}) => {
 
     const location = useLocation();
     const {city, lat, lon, name, state, formatted, website, datasource: {raw: { email }} }  = location.state;
     const [userFormattedAdd, setUserFormattedAdd] = useState([]);
+    const [direction, setDirection] = useState([]);
 
-    const Item = styled(Paper)(() => ({
-       
+    const Item = styled(Paper)(() => ({     
         textAlign: 'left',
-        border: '1px solid gray',
-      
+        border: '1px solid gray', 
       }));
 
 
-      useEffect(() => {
-        
-           async function loadAddress(){
-                
+      useEffect(() => {    
+           async function loadAddress(){   
                 const res = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latLng.latitude}&lon=${latLng.longitude}&format=json&apiKey=8d3bbb9cb56248728d4751eb24464a1b`);
                 setUserFormattedAdd(res.data.results[0]);
             }
@@ -37,18 +40,26 @@ const SelectHospital = ({latLng}) => {
 
 
       useEffect(() => {
+        if (Object.keys(latLng).length > 0){
         const GEO_ROUTING_API = `https://api.geoapify.com/v1/routing?waypoints=${latLng.latitude},${latLng.longitude}|${lat},${lon}&mode=drive&apiKey=8d3bbb9cb56248728d4751eb24464a1b`;
         axios.get(GEO_ROUTING_API).then(res => {
-            console.log(res.data.features)
+            const featuresArr = (res.data.features[0].properties.legs[0].steps);
+            const routeText = [];
+            featuresArr.map((feature) => { 
+                routeText.push(feature.instruction.text)    
+            });
+            setDirection(routeText);
         })
+        }
       }, [latLng])
+      
       
 
     return (
         <div>
-            <Box sx={{ width: '90%', marginTop: 10 }}>
-            <Grid container spacing={1}>
-                <Grid item xs={6}> 
+            <Box style={{ maxWidth: '80%', marginTop: 30 }}>
+            <Grid container spacing={10} marginLeft={30}>
+                <Grid item > 
                      <Item style={{height: 'auto', maxWidth: 500, padding: 20}}>
                         {
                             <div>
@@ -76,8 +87,34 @@ const SelectHospital = ({latLng}) => {
                         }
                      </Item>
                 </Grid>
-                <Grid item xs={6}>
-                    <Item style={{height: 'auto', maxWidth: 500, padding: 20}} >2</Item>
+                <Grid item >
+                    <Item style={{height: 'auto', maxWidth: 500, padding: 0}} >
+                    <div>
+                        {
+                            direction.map((text, index) => {
+                                return(
+                                    <div key={index}>
+                                        <Timeline
+                                            sx={{
+                                            [`& .${timelineItemClasses.root}:before`]: {
+                                                    flex: 0,
+                                                    padding: 0,
+                                            },
+                                            }}
+                                        >
+                                        <TimelineItem>
+                                            <TimelineSeparator>
+                                            <TimelineDot />                                           
+                                            </TimelineSeparator>
+                                            <TimelineContent>{text}</TimelineContent>
+                                        </TimelineItem>   
+                                        </Timeline>    
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>  
+                    </Item>
                 </Grid>
             </Grid>
             </Box>
